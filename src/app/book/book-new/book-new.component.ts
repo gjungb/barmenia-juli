579fe0dce1085b28fcd422ddc8e0f5b3aa1966f4
Gerd Jungbluth <gerd.jungbluth@engawa.de>
2021-08-03 13:13:42 +0200

    ende tag 5

---
+++ src/app/book/book-new/book-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'barm-book-new',
+  templateUrl: './book-new.component.html',
+  styleUrls: ['./book-new.component.scss']
+})
+export class BookNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
